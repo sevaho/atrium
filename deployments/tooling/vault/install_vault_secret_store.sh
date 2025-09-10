6145343f1cd0138c13a12c322142045fd52bc495
sevaho <sebastiaan@sevaho.io>
2025-09-10 20:28:37 +0200

    Update external secrets store to a cluster secret store for easier config

--- deployments/tooling/vault/install_vault_secret_store.sh
+++ deployments/tooling/vault/install_vault_secret_store.sh
@@ -1,40 +1,51 @@
 # export VAULT_ADDR=XXX
 
-NAMESPACE=applications
-SECRET=external-secrets-sa-secret
+# ==========================================================
+# FOLLOWING SHOULD BE DONE ONLY ONCE TO CONFIGURE KUBE AUTH
+# ==========================================================
 
-# Enable kubernetes in vault
+
+# Enable kubernetes in vault (should be done only once)
 # vault auth enable kubernetes
 
-# Get the correct env vars
-TOKEN_REVIEW_JWT=$(kubectl -n $NAMESPACE get secret $SECRET -o jsonpath={.data.token} | base64 -d)
-KUBE_CA_CERT=$(kubectl config view --raw --minify --flatten -o jsonpath='{.clusters[].cluster.certificate-authority-data}' | base64 --decode)
-KUBE_HOST=$(kubectl config view --raw --minify --flatten --output='jsonpath={.clusters[].cluster.server}')
+# NAMESPACE=tooling
+# SECRET=external-secrets-sa-secret
+#
+# # Get the correct env vars
+# TOKEN_REVIEW_JWT=$(kubectl -n $NAMESPACE get secret $SECRET -o jsonpath={.data.token} | base64 -d)
+# KUBE_CA_CERT=$(kubectl config view --raw --minify --flatten -o jsonpath='{.clusters[].cluster.certificate-authority-data}' | base64 --decode)
+# KUBE_HOST=$(kubectl config view --raw --minify --flatten --output='jsonpath={.clusters[].cluster.server}')
+#
+# echo TOKEN:
+# echo $TOKEN_REVIEW_JWT
+# echo TOKEN CA CERT:
+# echo $KUBE_CA_CERT
+# echo KUBE HOST:
+# echo $KUBE_HOST
+#
+# # Write the token to vault, so that the token is allowed
+# # This is in essence the master key
+# vault write auth/kubernetes/config token_reviewer_jwt="$TOKEN_REVIEW_JWT" kubernetes_host="$KUBE_HOST" kubernetes_ca_cert="$KUBE_CA_CERT"
 
-echo TOKEN:
-echo $TOKEN_REVIEW_JWT
-echo TOKEN CA CERT:
-echo $KUBE_CA_CERT
-echo KUBE HOST:
-echo $KUBE_HOST
+# ==========================================================
+# TOOLING ACCESS
+# ==========================================================
 
-# Write the token to vault, so that the token is allowed
-vault write auth/kubernetes/config token_reviewer_jwt="$TOKEN_REVIEW_JWT" kubernetes_host="$KUBE_HOST" kubernetes_ca_cert="$KUBE_CA_CERT"
+NAMESPACES=tooling,applications
 
-# Create a policy for external-secrets
+# Create a policy
 vault policy write external-secrets-policy - <<EOF
-path "applications/data/*" {
+path "kubernetes/data/*" {
   capabilities = ["read"]
 }
-path "applications/metadata/*" {
+path "kubernetes/metadata/*" {
   capabilities = ["list", "read"]
 }
 EOF
 
 # Create a Kubernetes auth role
-vault delete auth/kubernetes/role/external-secrets-vault-role
 vault write auth/kubernetes/role/external-secrets-vault-role \
     bound_service_account_names=external-secrets-sa \
-    bound_service_account_namespaces=$NAMESPACE \
+    bound_service_account_namespaces=$NAMESPACES \
     policies=external-secrets-policy \
     ttl=24h
